e4618723001feffbe7c37ea9929efc6f9d1a9846
koshigoe <koshigoeb@gmail.com>
2015-04-07 21:47:32 +0900

    initial commit.

---
+++ lib/capistrano3/ec2/minimal.rb
@@ -0,0 +1,9 @@
+require "capistrano3/ec2/minimal/version"
+
+module Capistrano3
+  module Ec2
+    module Minimal
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano3/ec2/minimal/version.rb
@@ -0,0 +1,7 @@
+module Capistrano3
+  module Ec2
+    module Minimal
+      VERSION = "0.1.0"
+    end
+  end
+end
